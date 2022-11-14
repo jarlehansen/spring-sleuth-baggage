fda8f75fe398a120e92c1a93f0f4d42a6bc4a49f
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-11-14 09:38:01 +0100

    updated java example

--- src/main/java/com/example/TestConfig.java
+++ src/main/java/com/example/TestConfig.java
@@ -12,9 +12,9 @@ public class TestConfig {
     @Bean(HttpServerRequestParser.NAME)
     public HttpRequestParser sleuthHttpServerRequestParser(Tracer tracer) {
         return (request, context, span) -> {
-            tracer.createBaggage("1", "1");
-            tracer.createBaggage("2", "2");
-            tracer.createBaggage("3", "3");
+            tracer.createBaggage("1").set(context, "1");
+            tracer.createBaggage("2").set(context, "2");
+            tracer.createBaggage("3").set(context, "3");
         };
     }
 
